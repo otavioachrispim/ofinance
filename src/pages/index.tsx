9bb56913697444aa754337690b2e4eba366ea789
unknown <otavio.paiva93@yahoo.com.br>
2021-07-24 10:28:56 -0300

    SignIn stylized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,48 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Button, Stack, Image } from '@chakra-ui/react';
 
-export default function Home() {
+import {Input} from '../components/Form/Input';
+
+export default function SignIn() {
   return (
     <Flex 
       w="100vw"
       h="100vh"
       align="center"
       justify="center"
+      flexDir="column"   
     >
-      <h1>Ola</h1>
+      
+
+      <Flex
+       as="form"
+       w="100%" 
+       maxWidth={420}
+       bg="white"
+       p="10"
+       borderRadius={20}
+       flexDir="column"       
+      >
+        <Image 
+          src="/images/logo.svg"
+          alt="Logo" 
+          objectFit="contain"
+          mb="4"
+      />
+
+        <Stack spacing="4">
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Senha" />            
+        </Stack>
+        
+          <Button 
+            colorScheme="green"
+            type="submit"
+            size="lg"
+            mt="8"
+          >
+            Entrar
+          </Button>
+      </Flex>
     </Flex>
   )
 }
